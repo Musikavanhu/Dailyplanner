b8299cb29900d957e88891a2c5d52fd07cce204f
Tinotenda Musikavanhu <thesafehavenhero@gmail.com>
2021-06-15 19:21:12 -0500

    Update created

--- script.js
+++ script.js
@@ -1,132 +1,53 @@
-$(document).ready(function() {
-  
-//   Time
-    const test = false;
-   const now = moment().format('MMMM Do YYYY');
-   let nowHour24 = moment().format('H');
-    let nowHour12 = moment().format('h');
-  if (test) {
-      nowHour24 = 13;
-      nowHour12 = 1;
-    }
-  
-    let $dateHeading = $('#navbar-subtitle');
-    $dateHeading.text(now);
-    
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
+var time = moment();
 
-    let storedTasks = JSON.parse(localStorage.getItem("storedTasks"));
-  if (test) { console.log(s); }
-  if (storedTasks !== null) {
-      planTextArr = storedTasks;
-    } else {
-    planTextArr = new Array(9);
-}
-  
-    if (test) { console.log("full array of plned text",planTextArr); }
-  let $plannerDiv = $('#plannerContainer');
-  $plannerDiv.empty();
-  if (test) { console.log("current time",nowHour12); }
-for (let hour = 9; hour <= 17; hour++) {
-let index = hour - 9;
+function setPlanner() {
 
-let $rowDiv = $('<div>');
-      $rowDiv.addClass('row');
-      $rowDiv.addClass('plannerRow');
-      $rowDiv.attr('hour-index',hour);
-let $col2TimeDiv = $('<div>');
-      $col2TimeDiv.addClass('col-md-2');
-    
-      const $timeBoxSpn = $('<span>');
-   
-      $timeBoxSpn.attr('class','timeBox');
-let displayHour = 0;
-      let ampm = "";
-      if (hour > 12) { 
-        displayHour = hour - 12;
-        ampm = "pm";
-      } else {
-        displayHour = hour;
-        ampm = "am";
-      }
-$timeBoxSpn.text(`${displayHour} ${ampm}`);
-  
-      $rowDiv.append($col2TimeDiv);
-      $col2TimeDiv.append($timeBoxSpn);
-   
-      let $dailyPlanSpn = $('<input>');
-  
-      $dailyPlanSpn.attr('id',`input-${index}`);
-      $dailyPlanSpn.attr('hour-index',index);
-      $dailyPlanSpn.attr('type','text');
-      $dailyPlanSpn.attr('class','dailyPlan');
-  
-      $dailyPlanSpn.val( planTextArr[index] );
-  
-      let $col9IptDiv = $('<div>');
-      $col9IptDiv.addClass('col-md-9');
-  
-   
-      $rowDiv.append($col9IptDiv);
-      $col9IptDiv.append($dailyPlanSpn);
-   
-      let $col1SaveDiv = $('<div>');
-      $col1SaveDiv.addClass('col-md-1');
-  
-      let $saveBtn = $('<i>');
-      $saveBtn.attr('id',`saveid-${index}`);
-      $saveBtn.attr('save-id',index);
-      $saveBtn.attr('class',"far fa-save saveIcon");
-      
-      $rowDiv.append($col1SaveDiv);
-      $col1SaveDiv.append($saveBtn);
-   
-      updateRowColor($rowDiv, hour);
-      
-      $plannerDiv.append($rowDiv);
-    };
-   function updateRowColor ($hourRow,hour) { 
-  
-      if (test) { console.log("rowColor ",nowHour24, hour); }
-  
-      if ( hour < nowHour24) {
-   
-        if (test) { console.log("lessThan"); }
-        $hourRow.css("background-color","lightgrey")
-      } else if ( hour > nowHour24) {
-        if (test) { console.log("greaterthan"); }
-        $hourRow.css("background-color","lightgreen")
-      } else {
-        if (test) { console.log("eqaul"); }
-        $hourRow.css("background-color","tomato")
-      }
-    };
-  
- 
-    $(document).on('click','i', function(event) {
-      event.preventDefault();  
-  
-      if (test) { console.log('click pta before '+ planTextArr); }
-  
-      let $index = $(this).attr('save-id');
-  
-      let inputId = '#input-'+$index;
-      let $value = $(inputId).val();
-  
-      planTextArr[$index] = $value;
-  
-  
-      if (test) { console.log('value ', $value); }
-      if (test) { console.log('index ', $index); }
-      if (test) { console.log('click pta after '+ planTextArr); }
-  
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-      $(`#saveid-${$index}`).removeClass('shadowPulse');
-      localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
-    });  
-    
-    
-      let i = $(this).attr('hour-index');
-     
-      
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
     });
-;  
+}
+
+setPlanner();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
